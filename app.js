0667b0537769eb652e0c1cfce81592d218a39538
kate <katecr17@gmail.com>
2021-09-13 12:45:45 -0500

    receive parameter when invoking app.js and do list action

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
 
-let data = fs.readFileSync(__dirname + "/tasks.json", "utf-8");
-
-console.log(data);
+//Analyze the size of the array of arguments, default is 2 items, validate if it does not have 3, it is because no parameter was sent
+if (process.argv.length < 3) {
+  console.log("Atención - Tienes que pasar una acción");
+} else {
+  let option = process.argv[2]; //the parameter sent is located in position 3 and is assigned to a variable
+  switch (option) {
+    case "listar":
+      fs.readFile(__dirname + "/tasks.json", "utf-8", (err, data) => {
+        if (err) throw err;
+        let task = JSON.parse(data);
+        console.log(task);
+      });
+      break;
+    default:
+      console.log("No entiendo qué quieres hacer.");
+      break;
+  }
+}
